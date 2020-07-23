78f32d3d1015dd957247954dac2eff4ed7a8bf71
ZuhraB <zuhra.bakhshi.com>
2020-07-23 01:44:05 +0000

    new file

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    console.log( `😍Assertion Passed: ${actual}  === ${expected}`)
+  }
+    else{
+      console.log(`🥺Assertion Failed:  ${actual}  !==   ${expected}`)
+    }
+
+};
+const countLetters = function(letters){
+  letters = letters.replace(/\s/g, '')
+  var result = {}
+  for(var i of letters){
+    if(!result[i]){
+      result[i] = 1
+    }
+    else{
+      result[i] += 1 
+    }
+    
+  }
+  return result
+  
+}
+assertEqual(countLetters("lighthouse in thehouse"), {l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1})
+assertEqual({},{})
+ 
